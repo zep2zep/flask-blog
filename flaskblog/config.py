c0156a33c5883d2aff10dbcf096b1d1d90032d1a
unknown <your email>
2024-01-01 15:58:06 +0100

    Nev pwd google

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -19,5 +19,5 @@ class Config:
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("EMAIL_USER")
-    MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
+    MAIL_USERNAME = os.environ.get("EMAIL_HOST_USER")
+    MAIL_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
